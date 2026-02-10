e4120ff70f42fb79270e4dfda0d2680d9cd75ea4
gesya-1607 <gesyaputri802@gmail.com>
2026-02-10 09:31:50 +0700

    Add files via upload

---
+++ auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'SECRET_KEY',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    PrismaService,
+    JwtStrategy, // ← WAJIB agar JWT jalan
+  ],
+})
+export class AuthModule {}
